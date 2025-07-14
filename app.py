2a1ec2234c421e37f94fe12036eeafa5e3472e5f
GloiusL <fantasy.medieval.world@gmail.com>
2025-07-14 16:06:44 +0300

    Добавлены маршруты для robots.txt и sitemap.xml

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(os.getcwd(), 'robots.txt')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.getcwd(), 'sitemap.xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
